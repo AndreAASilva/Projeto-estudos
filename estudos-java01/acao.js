3f5eb68b5aa21bfc7600197e1964e2fe477517e5
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2022-08-12 04:49:08 -0300

    Sintaxe JavaScript

---
+++ estudos-java01/acao.js
@@ -0,0 +1,31 @@
+function apagado(){
+    document.getElementById("minhaImagem").src='lampada_apagada.gif'
+}
+
+function aceso(){
+    document.getElementById("minhaImagem").src='lampada_acesa.gif'
+}
+
+function soma(){
+    document.getElementById('demo').innerHTML = 5 + 5
+}
+
+function apaga(){
+    document.getElementById('demo').innerHTML = ' '
+}
+
+document.write(5+7)
+
+document.getElementById('demo2').innerHTML = 'Inseriu o texto quando atualiza a página'
+
+let a, b, c
+a = 5
+b = 3
+c = a*b
+
+document.getElementById('demo3').innerHTML = c
+
+function meuCodigo(){
+    document.getElementById('demo4').innerHTML = 'Olá Mundo!'
+    document.getElementById('demo5').innerHTML = 'Hello World!'
+}
